0b31df8af7b91d2f0bf4d3c17351f94d02292df5
tpk73 <tpaaka73@gmail.com>
2020-11-17 16:38:10 -0600

    WATiR initial commit

---
+++ LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  WATiR
+//
+//  Created by Truett Knox on 11/17/20.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
--- WATiR/ViewController.swift
+++ WATiR/ViewController.swift
@@ -2,18 +2,54 @@
 //  ViewController.swift
 //  WATiR
 //
-//  Created by Truett Knox on 11/17/20.
+//  Created by Tanner Parker on 11/17/20.
 //
 
 import UIKit
+import FSCalendar
 
 class ViewController: UIViewController {
 
+    fileprivate weak var calendar: FSCalendar?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //Calendar SetUp
+        let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
+        calendar.dataSource = self
+        calendar.delegate = self
+        calendar.register(FSCalendarCell.self, forCellReuseIdentifier: "Cell")
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(calendar)
+        
+        calendar.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        calendar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        calendar.heightAnchor.constraint(equalToConstant: 275).isActive = true
+        calendar.widthAnchor.constraint(equalToConstant: view.frame.width - 40).isActive = true
+        
+        self.calendar = calendar
     }
-
-
+    
 }
 
+extension ViewController: FSCalendarDataSource, FSCalendarDelegate{
+//    func calendar(_ calendar: FSCalendar, titleFor date: Date) -> String? {
+//        return "maxcodes.io"
+//    }
+    
+//    func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
+//        return "subscribe"
+//    }
+//    func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
+//        return UIImage(named: "turtlerock")
+//    }
+    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
+        let cell = calendar.dequeueReusableCell(withIdentifier: "Cell", for: date, at: position)
+        cell.imageView.contentMode = .scaleAspectFit
+        return cell
+    }
+    
+}
